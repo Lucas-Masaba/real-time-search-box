be11a7a3f711eb6aa337595a7e5b70f98e9c07f3
Lucas-Masaba <masabaluke@gmail.com>
2022-12-01 21:15:04 +0300

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_102503) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_173257) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "articles"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,5 +26,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_102503) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "articles", "users"
 end
