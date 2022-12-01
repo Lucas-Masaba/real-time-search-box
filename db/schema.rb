930205522cda5ccd7a1da679190700d20ef2618d
Lucas-Masaba <masabaluke@gmail.com>
2022-12-01 13:30:03 +0300

    create articles table and add foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_220206) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_102503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "articles"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_220206) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
 end
